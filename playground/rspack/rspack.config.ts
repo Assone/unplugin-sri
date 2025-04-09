a3e723e7a896192ad0b4ad6a20ab02e410d3cc1a
Assone <14324389+Assone@users.noreply.github.com>
2025-04-10 01:34:03 +0800

    fix: eslint error

--- playground/rspack/rspack.config.ts
+++ playground/rspack/rspack.config.ts
@@ -6,62 +6,62 @@ import Unplugin from '../../src/rspack'
 const targets = ['chrome >= 87', 'edge >= 88', 'firefox >= 78', 'safari >= 14']
 
 export default defineConfig({
-	entry: {
-		main: './src/index.ts',
-	},
-	resolve: {
-		extensions: ['...', '.ts'],
-	},
-	module: {
-		rules: [
-			{
-				test: /\.svg$/,
-				type: 'asset',
-			},
-			{
-				test: /\.js$/,
-				use: [
-					{
-						loader: 'builtin:swc-loader',
-						options: {
-							jsc: {
-								parser: {
-									syntax: 'ecmascript',
-								},
-							},
-							env: { targets },
-						},
-					},
-				],
-			},
-			{
-				test: /\.ts$/,
-				use: [
-					{
-						loader: 'builtin:swc-loader',
-						options: {
-							jsc: {
-								parser: {
-									syntax: 'typescript',
-								},
-							},
-							env: { targets },
-						},
-					},
-				],
-			},
-		],
-	},
-	plugins: [new rspack.HtmlRspackPlugin({ template: './index.html' }), Unplugin()],
-	optimization: {
-		minimizer: [
-			new rspack.SwcJsMinimizerRspackPlugin(),
-			new rspack.LightningCssMinimizerRspackPlugin({
-				minimizerOptions: { targets },
-			}),
-		],
-	},
-	experiments: {
-		css: true,
-	},
+  entry: {
+    main: './src/index.ts',
+  },
+  resolve: {
+    extensions: ['...', '.ts'],
+  },
+  module: {
+    rules: [
+      {
+        test: /\.svg$/,
+        type: 'asset',
+      },
+      {
+        test: /\.js$/,
+        use: [
+          {
+            loader: 'builtin:swc-loader',
+            options: {
+              jsc: {
+                parser: {
+                  syntax: 'ecmascript',
+                },
+              },
+              env: { targets },
+            },
+          },
+        ],
+      },
+      {
+        test: /\.ts$/,
+        use: [
+          {
+            loader: 'builtin:swc-loader',
+            options: {
+              jsc: {
+                parser: {
+                  syntax: 'typescript',
+                },
+              },
+              env: { targets },
+            },
+          },
+        ],
+      },
+    ],
+  },
+  plugins: [new rspack.HtmlRspackPlugin({ template: './index.html' }), Unplugin()],
+  optimization: {
+    minimizer: [
+      new rspack.SwcJsMinimizerRspackPlugin(),
+      new rspack.LightningCssMinimizerRspackPlugin({
+        minimizerOptions: { targets },
+      }),
+    ],
+  },
+  experiments: {
+    css: true,
+  },
 })
